273040d5b6f4f1741f637811931f8779ca1d75fe
Davron Akil <davron@cyber-ny.com>
2015-11-12 21:23:44 -0500

    Initial Commit

---
+++ get_quote.php
@@ -0,0 +1,31 @@
+<html>
+<head>
+<style type="text/css">
+.GoogleForm iframe {
+    position: absolute;
+    top:0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    min-height: 2400px;
+
+}
+</style>
+
+<title>Get a Free Quote | PixelKraft Studios</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+
+<body>
+
+
+
+
+            <div class="GoogleForm">
+
+<iframe src="https://docs.google.com/forms/d/1aGBNORJ2oRsVwjblzfqPkiUrL0gUtcIZtE1pSlqWVcg/viewform" width="760" height="2200" frameborder="0" marginheight="0" marginwidth="5">Loading...</iframe>
+            </div>
+
+
+</body>
+</html>
